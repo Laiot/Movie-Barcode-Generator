5b2b4e7bcc75cf77eb0f0af7cf0c6e7df740878a
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2019-09-26 17:30:46 +0200

    Update Barcode_Generator.py

--- Barcode_Generator.py
+++ Barcode_Generator.py
@@ -14,14 +14,15 @@ def frame_avg(img):
     avgsq = np.average(squared, axis=1)
     return np.sqrt(avgsq).astype('uint8')
 
+
 def movie_iter(movie_name, frames_to_skip):
-    movie=cv2.VideoCapture(f"{movie_name}.mp4") #
+    movie = cv2.VideoCapture(f"{movie_name}.mp4")
     s, f = movie.read()
     while s:
         yield f
         for i in range(frames_to_skip):
             movie.read()
-        s,f = movie.read()
+        s, f = movie.read()
 
 
 def elab(movie_it):
@@ -30,12 +31,11 @@ def elab(movie_it):
 
     return res
 
-complete = collect_frames(movie)
 
-c = np.array(complete)
+it = movie_iter("movies/SampleVideo_1280x720_1mb.mp4", 4)
 
+res = elab(it)
+c = np.array(res)
 cc = c.swapaxes(0, 1)
-
 i = Image.fromarray(cc, mode='RGB')
-
-i.save('multip_test_steven.jpg')
+i.save("prova_parall.jpg")
